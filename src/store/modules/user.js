000f3a13616cf01c0b0878803c481b628eb673c2
Ones <964981348@qq.com>
2020-05-05 12:54:58 +0800

    添加文档

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -42,7 +42,19 @@ const user = {
           Vue.ls.set(ACCESS_TOKEN, result.token, 7 * 24 * 60 * 60 * 1000)
           Vue.ls.set(PERMISSION, Array.isArray(result.role) ? result.role : result.role.split(','))
           Vue.ls.set(USER_INFO, result)
-          // TODO:1.mock移到server端 2.修改axios 6.减小clone的包大小 7.调研是否可以在生产模式中动态修改主题颜色(理论上可以因为可以动态修改色弱模式),并且将这个按钮放在用户设置中
+          // TODO:1.mock移到server端
+          // 2.修改axios
+          // 3.再次精简路由
+          // 6.减小clone的包大小
+          // 7.调研是否可以在生产模式中动态修改主题颜色(理论上可以因为可以动态修改色弱模式),并且将这个按钮放在用户设置中
+
+          // 已完成内容:
+          // 本系统基于ant deign pro vue 官方精简完成, 基于2.0.2tag, 所缺少的文件都可以通过拷贝源文件复制过来
+          // 本系统分为两个版本,一般精简(master)和极度精简(simplify),其中一般精简只修改了路由逻辑,按照权限分配(admin,common uer),而不是原版的按照菜单分配(table,list等等),极度精简版就是精简一切能精简的功能
+          // 精简文件
+          // 精简路由(去掉了异步路由的过程,也不需要在router.beforeEach中动态加载)
+          // 国际化
+          // 精简登录 (删除手机号登录内容,需要的话可以从官方2.0.2tag复制)
           commit('SET_TOKEN', result.token)
           resolve()
         }).catch(error => {
